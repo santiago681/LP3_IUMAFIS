b3ff43fc91ad99e915d697e7f80ad31ba72ca77d
santiago681 <scano9608@gmail.com>
2026-04-16 09:50:49 -0500

    Cambios Ejercicios

---
+++ SistemaEstudiantes/src/main/java/co/edu/iumafis/lang3/SistemaEstudiantes.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package co.edu.iumafis.lang3;
+
+/**
+ *
+ * @author ASUS
+ */
+public class SistemaEstudiantes {
+
+    String nombre = "Laura Gomez"
+    }
+
