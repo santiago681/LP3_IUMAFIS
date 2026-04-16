d8a7dae73e49946bbff77b678e217f40ec57573d
santiago681 <scano9608@gmail.com>
2026-04-16 09:54:07 -0500

    commit

--- SistemaEstudiantes/src/main/java/co/edu/iumafis/lang3/SistemaEstudiantes.java
+++ SistemaEstudiantes/src/main/java/co/edu/iumafis/lang3/SistemaEstudiantes.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package co.edu.iumafis.lang3;
 
-/**
- *
- * @author ASUS
- */
+
 public class SistemaEstudiantes {
 
     String nombre = "Laura Gomez"
